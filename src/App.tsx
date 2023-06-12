0ef0de5e7e16b73e4d2832f245bad0b7c0137c3f
Anna Escolaro <annaescolaro@hotmail.com>
2023-06-12 20:22:55 -0300

    req 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './styles/App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { About, NotFound, Posts, Users } from './pages';
 
 function App() {
-  return (<p>WIP</p>);
+  const router = createBrowserRouter([{
+    path: '/',
+    element: <Users />,
+  },
+  {
+    path: '/posts/:id',
+    element: <Posts />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/*',
+    element: <NotFound />,
+  },
+  ]);
+
+  return (
+    <RouterProvider router={ router } />
+  );
 }
 
 export default App;
